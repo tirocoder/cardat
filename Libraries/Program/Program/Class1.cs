6550de930f5e7b513bfc2bc625ee5791a44f843f
beemwe@gmx.at <beemwe@gmx.at@14063784-4365-08d0-5047-28e406753ca6>
2011-08-22 19:12:29 +0000

    initial checkin

---
+++ Libraries/Program/Program/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    public class Program
+    {
+        int vehicle_counter, program_start_counter;
+        string[] vehicles = new string[100];
+
+        public void 
+    }
+}
---
+++ Libraries/WindowsApplication1/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("WindowsApplication1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard")> 
+<Assembly: AssemblyProduct("WindowsApplication1")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("f01054f3-8cf8-44c8-b619-69af3b37ad56")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
